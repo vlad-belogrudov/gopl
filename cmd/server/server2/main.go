0df1e274816c09225e06f65b711cc74adc722c75
Vladislav Belogrudov <vlad.belogrudov@gmail.com>
2021-03-29 19:20:16 +0300

    Parametrized Lissajous server

--- cmd/server/server2/main.go
+++ cmd/server/server2/main.go
@@ -3,13 +3,67 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/vlad-belogrudov/gopl/pkg/lissajous"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		lissajous.Lissajous(w)
+		var err error
+		if err = r.ParseForm(); err != nil {
+			log.Println("cannot parse params: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		params := lissajous.DefaultLissajousParams()
+		for k, v := range r.Form {
+			switch k {
+			case "cycles":
+				params.Cycles, err = strconv.Atoi(v[0])
+				if err != nil {
+					log.Println("cannot parse cycles: ", err)
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+			case "res":
+				params.Res, err = strconv.ParseFloat(v[0], 10)
+				if err != nil {
+					log.Println("cannot parse res: ", err)
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+			case "xsize":
+				params.Xsize, err = strconv.Atoi(v[0])
+				if err != nil {
+					log.Println("cannot parse xsize: ", err)
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+			case "ysize":
+				params.Ysize, err = strconv.Atoi(v[0])
+				if err != nil {
+					log.Println("cannot parse ysize: ", err)
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+			case "frames":
+				params.Frames, err = strconv.Atoi(v[0])
+				if err != nil {
+					log.Println("cannot parse frames: ", err)
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+			case "delay":
+				params.Delay, err = strconv.Atoi(v[0])
+				if err != nil {
+					log.Println("cannot parse delay ", err)
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+			}
+		}
+		lissajous.Lissajous(w, params)
 	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
--- pkg/lissajous/lissajous.go
+++ pkg/lissajous/lissajous.go
@@ -38,29 +38,45 @@ var palette = []color.Color{color.Black,
 	},
 }
 
-func Lissajous(out io.Writer) {
-	const (
-		cycles  = 5
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
-	)
+type LissajousParams struct {
+	Cycles int
+	Frames int
+	Xsize  int
+	Ysize  int
+	Res    float64
+	Delay  int
+}
+
+func DefaultLissajousParams() LissajousParams {
+	return LissajousParams{
+		Cycles: 5,
+		Res:    0.001,
+		Xsize:  100,
+		Ysize:  100,
+		Frames: 64,
+		Delay:  8,
+	}
+}
+
+func Lissajous(out io.Writer, params LissajousParams) {
 	rand.Seed(time.Now().UTC().UnixNano())
 	colorIndex := uint8(rand.Intn(len(palette)-1) + 1)
 	freq := rand.Float64() * 3.0
-	anim := gif.GIF{LoopCount: nframes}
+	anim := gif.GIF{LoopCount: params.Frames}
 	phase := 0.0
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+	for i := 0; i < params.Frames; i++ {
+		rect := image.Rect(0, 0, 2*params.Xsize+1, 2*params.Ysize+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(params.Cycles*2)*math.Pi; t += params.Res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), colorIndex)
+			img.SetColorIndex(
+				params.Xsize+int(x*float64(params.Xsize)+0.5),
+				params.Ysize+int(y*float64(params.Ysize)+0.5),
+				colorIndex)
 		}
 		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
+		anim.Delay = append(anim.Delay, params.Delay)
 		anim.Image = append(anim.Image, img)
 	}
 	gif.EncodeAll(out, &anim)
