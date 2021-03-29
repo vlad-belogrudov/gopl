44e1d93470f3f9ac9c2a39e3edc0dad80f50f644
Vladislav Belogrudov <vlad.belogrudov@gmail.com>
2021-03-29 16:01:54 +0300

    Added fetchall

---
+++ cmd/fetch/fetchall/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	defer resp.Body.Close()
+	filename := strings.ReplaceAll(url, "http://", "")
+	filename = strings.ReplaceAll(filename, "https://", "")
+	filename = strings.ReplaceAll(filename, ":", "_")
+	out, err := os.Create(filename)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	defer out.Close()
+	nbytes, err := io.Copy(out, resp.Body)
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
