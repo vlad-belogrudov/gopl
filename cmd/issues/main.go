1fe746e4354b589483e93f2ed409c5abf474684d
Vladislav Belogrudov <vlad.belogrudov@gmail.com>
2021-05-29 18:21:56 +0300

    Github issue app - create new issue

--- cmd/issues/main.go
+++ cmd/issues/main.go
@@ -12,10 +12,10 @@ import (
 	"github.com/vlad-belogrudov/gopl/pkg/github"
 )
 
-func main() {
-	result, err := github.SearchIssues(os.Args[1:])
+func search(terms []string) error {
+	result, err := github.SearchIssues(terms)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%d titles:\n", result.TotalCount)
 	sort.Slice(result.Items, func(i, j int) bool {
@@ -26,18 +26,67 @@ func main() {
 	color.Green("#### less than month ago ####")
 	for _, item := range result.Items {
 		if this_month {
-			if time.Now().Sub(item.CreatedAt).Hours() > 24*30 {
+			if time.Since(item.CreatedAt).Hours() > 24*30 {
 				this_month = false
 				color.Yellow("#### less than year ago ####")
 			}
 		}
 		if this_year {
-			if time.Now().Sub(item.CreatedAt).Hours() > 24*365 {
+			if time.Since(item.CreatedAt).Hours() > 24*365 {
 				this_year = false
 				color.Red("#### more than year ago ####")
 			}
 		}
-		fmt.Printf("#%-5d %s %9.9s %.55s\n",
+		fmt.Printf("#%-5d %s %20.20s %.55q\n",
 			item.Number, item.CreatedAt.Format("2006-01-02"), item.User.Login, item.Title)
+	}
+	return nil
+}
+
+func getToken() (string, error) {
+	const userPassVar = "GHUSERPASS"
+	login, ok := os.LookupEnv(userPassVar)
+	if !ok {
+		return "", fmt.Errorf("%s is not set", userPassVar)
+	}
+	return login, nil
+}
+
+func create(repo, title, text string) error {
+	token, err := getToken()
+	if err != nil {
+		return err
+	}
+	issue, err := github.CreateIssue(token, repo, title, text)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Created issue %d at %s (state: %s)\n",
+		issue.Number, issue.CreatedAt, issue.State)
+	return nil
+}
+
+func help() {
+	log.Fatalln("Usage: issues <create|update|close|search|show> [optinal terms]")
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		help()
+	}
+	var err error
+	switch os.Args[1] {
+	case "search":
+		err = search(os.Args[2:])
+	case "create":
+		if len(os.Args) != 5 {
+			log.Fatalln("need additional arguments - full repo name, title, text")
+		}
+		err = create(os.Args[2], os.Args[3], os.Args[4])
+	default:
+		help()
+	}
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
--- pkg/github/github.go
+++ pkg/github/github.go
@@ -1,15 +1,18 @@
 package github
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
 	"time"
 )
 
-const IssuesUrl = "https://api.github.com/search/issues"
+const searchIssuesUrl = "https://api.github.com/search/issues"
+const createIssueUrl = "https://api.github.com/repos/%s/issues"
 
 type IssuesSearchResult struct {
 	TotalCount int `json:"total_count"`
@@ -26,6 +29,17 @@ type Issue struct {
 	Body      string
 }
 
+type NewIssue struct {
+	Title string `json:"title"`
+	Text  string `json:"body"`
+}
+
+type NewIssueResult struct {
+	Number    int       `json:"number"`
+	State     string    `json:"state"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
@@ -34,15 +48,44 @@ type User struct {
 // SearchIssues asks Github for terms
 func SearchIssues(terms []string) (*IssuesSearchResult, error) {
 	q := url.QueryEscape(strings.Join(terms, " "))
-	resp, err := http.Get(IssuesUrl + "?q=" + q)
+	log.Println(q)
+	resp, err := http.Get(searchIssuesUrl + "?q=" + q)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("Failed request: %s", resp.Status)
+		return nil, fmt.Errorf("failed request: %s", resp.Status)
 	}
 	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+// CreateIssue creates new issue at Github repo
+func CreateIssue(token, repo, title, text string) (*NewIssueResult, error) {
+	issue := NewIssue{Title: title, Text: text}
+	data, err := json.Marshal(issue)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", fmt.Sprintf(createIssueUrl, repo), bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+	req.Header.Add("Authorization", "token "+token)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusCreated {
+		return nil, fmt.Errorf("failed request: %s", resp.Status)
+	}
+	var result NewIssueResult
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		return nil, err
 	}
